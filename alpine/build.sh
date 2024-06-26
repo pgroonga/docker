d0cf9e2d147f84ca9cfcd8ffb05e9a412f435b63
otegami <otegami@clear-code.com>
2024-06-26 16:59:34 +0900

    alpine build: remove the steps to build MeCab separately

--- alpine/build.sh
+++ alpine/build.sh
@@ -10,6 +10,11 @@ MECAB_VERSION=0.996
 mkdir build
 cd build
 
+wget https://packages.groonga.org/source/groonga/groonga-${GROONGA_VERSION}.tar.gz
+tar xf groonga-${GROONGA_VERSION}.tar.gz
+pushd groonga-${GROONGA_VERSION}
+
+cd vendor
 wget \
   -O mecab.tar.gz \
   https://packages.groonga.org/source/mecab/mecab-${MECAB_VERSION}.tar.gz
@@ -18,28 +23,14 @@ cd mecab-*
 sed -i.bak -e 's,ipadic,naist-jdic,g' mecabrc.in
 wget https://github.com/taku910/mecab/pull/71.patch
 patch -p2 < 71.patch
-autoreconf --force --install
-./configure --prefix=/usr/local
-make -j$(nproc)
-make install
 cd -
 
 wget \
   -O mecab-naist-jdic.tar.gz \
   https://packages.groonga.org/source/mecab-naist-jdic/mecab-naist-jdic-0.6.3b-20111013.tar.gz
 tar xf mecab-naist-jdic.tar.gz
-cd mecab-naist-jdic-*
-autoreconf --force --install
-./configure \
-  --prefix=/usr/local \
-  --with-charset=utf-8
-make -j$(nproc)
-make install
-cd -
+cd ..
 
-wget https://packages.groonga.org/source/groonga/groonga-${GROONGA_VERSION}.tar.gz
-tar xf groonga-${GROONGA_VERSION}.tar.gz
-cd groonga-${GROONGA_VERSION}
 cmake \
   -S . \
   -B ../groonga.build \
@@ -47,7 +38,7 @@ cmake \
   -DCMAKE_INSTALL_PREFIX=/usr/local
 cmake --build ../groonga.build
 cmake --install ../groonga.build
-cd -
+popd
 
 wget https://packages.groonga.org/source/pgroonga/pgroonga-${PGROONGA_VERSION}.tar.gz
 tar xf pgroonga-${PGROONGA_VERSION}.tar.gz
