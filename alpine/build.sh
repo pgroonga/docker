c4280d852c83497e893f082a0849872a54fdb3b5
otegami <otegami@clear-code.com>
2024-06-27 12:28:10 +0900

    Use push/popd instead of cd

--- alpine/build.sh
+++ alpine/build.sh
@@ -8,28 +8,28 @@ GROONGA_VERSION=$2
 MECAB_VERSION=0.996
 
 mkdir build
-cd build
+pushd build
 
 wget https://packages.groonga.org/source/groonga/groonga-${GROONGA_VERSION}.tar.gz
 tar xf groonga-${GROONGA_VERSION}.tar.gz
 pushd groonga-${GROONGA_VERSION}
 
-cd vendor
+pushd vendor
 wget \
   -O mecab.tar.gz \
   https://packages.groonga.org/source/mecab/mecab-${MECAB_VERSION}.tar.gz
 tar xf mecab.tar.gz
-cd mecab-*
+pushd mecab-*
 sed -i.bak -e 's,ipadic,naist-jdic,g' mecabrc.in
 wget https://github.com/taku910/mecab/pull/71.patch
 patch -p2 < 71.patch
-cd -
+popd
 
 wget \
   -O mecab-naist-jdic.tar.gz \
   https://packages.groonga.org/source/mecab-naist-jdic/mecab-naist-jdic-0.6.3b-20111013.tar.gz
 tar xf mecab-naist-jdic.tar.gz
-cd ..
+popd
 
 cmake \
   -S . \
@@ -42,10 +42,10 @@ popd
 
 wget https://packages.groonga.org/source/pgroonga/pgroonga-${PGROONGA_VERSION}.tar.gz
 tar xf pgroonga-${PGROONGA_VERSION}.tar.gz
-cd pgroonga-${PGROONGA_VERSION}
+pushd pgroonga-${PGROONGA_VERSION}
 make PGRN_DEBUG=yes HAVE_MSGPACK=1 MSGPACK_PACKAGE_NAME=msgpack-c -j$(nproc)
 make install
-cd -
+popd
 
-cd ..
+popd
 rm -rf build
