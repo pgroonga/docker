66fffebe8c3f4871b9713d9b9d5e2d08bef684e5
takuya kodama <otegami@clear-code.com>
2024-06-28 10:31:33 +0800

    alpine: build MeCab in Groonga build (#48)
    Fix #51
    
    Groonga can build [MeCab](https://taku910.github.io/mecab/) together
    with CMake.
    So we don't have to build MeCab separately in
    [alpine/build.sh](https://github.com/pgroonga/docker/blob/main/alpine/build.sh).
    
    ref: https://github.com/groonga/groonga/tree/main/vendor/mecab
    ref:
    https://github.com/groonga/groonga/blob/e5ecc368e7825621374d7b0c7662fa455ebd8dd3/CMakeLists.txt#L764-L769

--- alpine/build.sh
+++ alpine/build.sh
@@ -8,38 +8,16 @@ GROONGA_VERSION=$2
 MECAB_VERSION=0.996
 
 mkdir build
-cd build
-
-wget \
-  -O mecab.tar.gz \
-  https://packages.groonga.org/source/mecab/mecab-${MECAB_VERSION}.tar.gz
-tar xf mecab.tar.gz
-cd mecab-*
-sed -i.bak -e 's,ipadic,naist-jdic,g' mecabrc.in
-wget https://github.com/taku910/mecab/pull/71.patch
-patch -p2 < 71.patch
-autoreconf --force --install
-./configure --prefix=/usr/local
-make -j$(nproc)
-make install
-cd -
-
-wget \
-  -O mecab-naist-jdic.tar.gz \
-  https://packages.groonga.org/source/mecab-naist-jdic/mecab-naist-jdic-0.6.3b-20111013.tar.gz
-tar xf mecab-naist-jdic.tar.gz
-cd mecab-naist-jdic-*
-autoreconf --force --install
-./configure \
-  --prefix=/usr/local \
-  --with-charset=utf-8
-make -j$(nproc)
-make install
-cd -
+pushd build
 
 wget https://packages.groonga.org/source/groonga/groonga-${GROONGA_VERSION}.tar.gz
 tar xf groonga-${GROONGA_VERSION}.tar.gz
-cd groonga-${GROONGA_VERSION}
+pushd groonga-${GROONGA_VERSION}
+
+pushd vendor
+ruby download_mecab.rb
+popd
+
 cmake \
   -S . \
   -B ../groonga.build \
@@ -47,14 +25,14 @@ cmake \
   -DCMAKE_INSTALL_PREFIX=/usr/local
 cmake --build ../groonga.build
 cmake --install ../groonga.build
-cd -
+popd
 
 wget https://packages.groonga.org/source/pgroonga/pgroonga-${PGROONGA_VERSION}.tar.gz
 tar xf pgroonga-${PGROONGA_VERSION}.tar.gz
-cd pgroonga-${PGROONGA_VERSION}
+pushd pgroonga-${PGROONGA_VERSION}
 make PGRN_DEBUG=yes HAVE_MSGPACK=1 MSGPACK_PACKAGE_NAME=msgpack-c -j$(nproc)
 make install
-cd -
+popd
 
-cd ..
+popd
 rm -rf build
